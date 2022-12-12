f086a1b8fd6082d82d580fd182a33a5ab77fec0c
NathanSoa <nathansoares809@gmail.com>
2022-12-12 19:42:21 -0300

    Initial commit

---
+++ src/main/java/com/newgo/activity/supermarketapp/SupermarketAppApplication.java
@@ -0,0 +1,13 @@
+package com.newgo.activity.supermarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketAppApplication.class, args);
+    }
+
+}
