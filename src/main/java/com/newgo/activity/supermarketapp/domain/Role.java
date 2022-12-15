bbe116515c8f4801cc908ef2b2fb602f9d08cc3b
NathanSoa <nathansoares809@gmail.com>
2022-12-15 20:10:45 -0300

    refactor: add RoleName enumerator and implemented GrantedAuthority

--- src/main/java/com/newgo/activity/supermarketapp/domain/Role.java
+++ src/main/java/com/newgo/activity/supermarketapp/domain/Role.java
@@ -1,5 +1,40 @@
 package com.newgo.activity.supermarketapp.domain;
 
-public enum Role {
-    USER, ADMINISTRATOR
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+    @Override
+    public String getAuthority() {
+        return getRoleName().toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
 }
---
+++ src/main/java/com/newgo/activity/supermarketapp/domain/RoleName.java
@@ -0,0 +1,5 @@
+package com.newgo.activity.supermarketapp.domain;
+
+public enum RoleName {
+    ROLE_USER, ROLE_ADMINISTRATOR
+}
