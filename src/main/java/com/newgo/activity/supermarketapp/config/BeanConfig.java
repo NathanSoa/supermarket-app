301e683fe634b88aa7931f2f863d19ccc078e4ec
NathanSoa <nathansoares809@gmail.com>
2022-12-15 20:11:11 -0300

    refactor: add beans for security configuration

--- src/main/java/com/newgo/activity/supermarketapp/config/BeanConfig.java
+++ src/main/java/com/newgo/activity/supermarketapp/config/BeanConfig.java
@@ -1,14 +1,21 @@
 package com.newgo.activity.supermarketapp.config;
 
 import com.newgo.activity.supermarketapp.domain.Product;
-import com.newgo.activity.supermarketapp.domain.dto.ProductDTO;
+import com.newgo.activity.supermarketapp.entities.dto.ProductDTO;
+
+import com.newgo.activity.supermarketapp.utils.JwtTokenUtils;
 import org.apache.commons.lang3.ArrayUtils;
+
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Base64;
 
 @Configuration
@@ -27,5 +34,20 @@ public class BeanConfig {
                 });
 
         return modelMapper;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtTokenUtils jwtTokenUtils() {
+        return new JwtTokenUtils();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
