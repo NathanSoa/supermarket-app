499e53dcc7dec0589593a055bec2061402af37bf
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-12-16 16:40:29 -0300

    feat: add find by username method

--- src/main/java/com/newgo/activity/supermarketapp/repository/ProductItemRepository.java
+++ src/main/java/com/newgo/activity/supermarketapp/repository/ProductItemRepository.java
@@ -1,9 +1,14 @@
 package com.newgo.activity.supermarketapp.repository;
 
 import com.newgo.activity.supermarketapp.domain.ProductItem;
+import com.newgo.activity.supermarketapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
+
+    List<ProductItem> findAllByUser(User user);
 }
