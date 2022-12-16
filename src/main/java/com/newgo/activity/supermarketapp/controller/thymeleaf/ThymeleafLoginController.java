2e4426fc55a616f1b7ad26d7632f1c55c123d7d8
NathanSoa <nathansoares809@gmail.com>
2022-12-16 20:51:32 -0300

    feat: add login controller and page

---
+++ src/main/java/com/newgo/activity/supermarketapp/controller/thymeleaf/ThymeleafLoginController.java
@@ -0,0 +1,36 @@
+package com.newgo.activity.supermarketapp.controller.thymeleaf;
+
+import com.newgo.activity.supermarketapp.domain.RoleName;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class ThymeleafLoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
+
+    @RequestMapping("/success")
+    public void loginRedirect(HttpServletRequest request, HttpServletResponse response, Authentication authResult)  throws Exception {
+        String roles = authResult.getAuthorities().toString();
+
+        if(roles.contains(RoleName.ROLE_USER.toString()))
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/user/index"));
+        else if(roles.contains(RoleName.ROLE_ADMINISTRATOR.toString()))
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/admin/index"));
+    }
+}
