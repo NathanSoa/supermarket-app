90db3d70e872a5e7c7ae2dcde7fe8eee8d959a46
NathanSoa <nathansoares809@gmail.com>
2022-12-21 16:50:43 -0300

    fix: add exception handler to bad credentials exception

--- src/main/java/com/newgo/activity/supermarketapp/utils/exception/CustomExceptionHandler.java
+++ src/main/java/com/newgo/activity/supermarketapp/utils/exception/CustomExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.newgo.activity.supermarketapp.utils.exception;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.authentication.BadCredentialsException;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,8 +17,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({EmptyResultDataAccessException.class})
-    public ResponseEntity<Object> handleEmptyResultDataAcessException(EmptyResultDataAccessException e, WebRequest request) {
-        Error error = new Error(e.getClass().getName(), e.getMessage());
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException e, WebRequest request) {
+        Error error = new Error("Empty result data access exception", e.getMessage());
         return handleExceptionInternal(e, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler({BadCredentialsException.class})
+    public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException e, WebRequest request) {
+        Error error = new Error("Bad credentials exception", e.getMessage());
+        return handleExceptionInternal(e, error, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 }
